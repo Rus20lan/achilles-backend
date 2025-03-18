dc200c27dd8c9a8b235d467b85ea0dc157c0f6d9
Ruslan <ruslas@yandex.ru>
2025-03-18 17:30:40 +0300

    create model: Fact

---
+++ models/Fact.js
@@ -0,0 +1,36 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/db.js";
+
+const Fact = sequelize.define(
+  "Fact",
+  {
+    id: {
+      type: DataTypes.BIGINT,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    values: {
+      type: DataTypes.JSONB,
+      allowNull: false,
+      comment: "Поле хранит фактические данные по каждому объему",
+    },
+    volumeId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      comment: "Внешний ключ на таблицу volumes",
+      references: {
+        model: "volumes",
+        key: "id",
+      },
+      onDelete: "CASCADE",
+    },
+  },
+  {
+    tableName: "facts",
+    timestamps: true,
+    createdAt: "createdAt",
+    updatedAt: "updatedAt",
+  }
+);
+
+export default Fact;
--- models/associations.js
+++ models/associations.js
@@ -1,28 +1,33 @@
-import sequelize from '../config/db.js';
-import Title from './Title.js';
-import Resource from './Resource.js';
-import Volume from './Volume.js';
-import Design from './Design.js';
+import sequelize from "../config/db.js";
+import Title from "./Title.js";
+import Resource from "./Resource.js";
+import Volume from "./Volume.js";
+import Design from "./Design.js";
+import Fact from "./Fact.js";
 
 // Устанавливаем связь между Титул-Объем, как 1 ко многим
-Title.hasMany(Volume, { foreignKey: 'titleId', as: 'volumes' });
-Volume.belongsTo(Title, { foreignKey: 'titleId' });
+Title.hasMany(Volume, { foreignKey: "titleId", as: "volumes" });
+Volume.belongsTo(Title, { foreignKey: "titleId" });
 // Устанавливаем связь между Ресурс-Объем, как 1 ко многим
-Resource.hasMany(Volume, { foreignKey: 'resourceId' });
-Volume.belongsTo(Resource, { foreignKey: 'resourceId' });
+Resource.hasMany(Volume, { foreignKey: "resourceId" });
+Volume.belongsTo(Resource, { foreignKey: "resourceId" });
 
 // Устанавливаем связь между РД-Объем, как 1 ко многим
-Design.hasMany(Volume, { foreignKey: 'designId' });
-Volume.belongsTo(Design, { foreignKey: 'designId' });
+Design.hasMany(Volume, { foreignKey: "designId" });
+Volume.belongsTo(Design, { foreignKey: "designId" });
+
+// Устанавливаем связь между Volume и Fact, как 1 к 1
+Volume.hasOne(Fact, { foreignKey: "volumeId" });
+Fact.belongsTo(Volume, { foreignKey: "volumeId" });
 
 // Синхронизация моделей с базой данных
-// sequelize
-//   .sync()
-//   .then(() => {
-//     console.log('Таблицы успешно созданы!');
-//   })
-//   .catch((error) => {
-//     console.error('Ошибка при создании таблиц:', error);
-//   });
+sequelize
+  .sync()
+  .then(() => {
+    console.log("Таблицы успешно созданы!");
+  })
+  .catch((error) => {
+    console.error("Ошибка при создании таблиц:", error);
+  });
 
 export { Title, Resource, Volume };
