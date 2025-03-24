f60582e94b4831a92c53d413696038d5e0fd26e0
Ruslan <ruslas@yandex.ru>
2025-03-25 00:44:31 +0300

    add animation in BurgerBtn

--- client/src/components/burgerBtn/BurgerBtn.jsx
+++ client/src/components/burgerBtn/BurgerBtn.jsx
@@ -1,8 +1,20 @@
+import { useState } from 'react';
 import './style.scss';
 
 const BurgerBtn = () => {
+  const [isActive, setActive] = useState(false);
+  const handleClick = () => {
+    console.log('hello world');
+    setActive((isActive) => {
+      return !isActive;
+    });
+  };
+
   return (
-    <a className="burger_btn">
+    <a
+      className={`burger_btn ${isActive ? 'active' : ''}`}
+      onClick={handleClick}
+    >
       <b></b>
       <b></b>
       <b></b>
